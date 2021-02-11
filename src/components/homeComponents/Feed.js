b8e85741b2822b8be884abe7bc49541a0343176d
ErLarsen <erikskrautlarsen@gmail.com>
2021-02-11 23:36:24 +0100

    endret variabler så de samsvarer med respons

--- src/components/homeComponents/Feed.js
+++ src/components/homeComponents/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css";
 import { Card } from "react-bootstrap";
+import moment from 'moment'
 
 //Viser en "preview" av forumpostene
 //Vil gjerne bare vise de 3-4 første postene!!!!!!!!!!!1
@@ -10,9 +11,9 @@ const Feed = ({ post }) => {
         {post.map((post, i) => (
             <Card key={i}>
               <Card.Body>
-                <Card.Title>{post.title}<i><small> i Kategori</small></i></Card.Title>
+                <Card.Title>{post.post_Title}<i><small> i {post.subTopic_Title}</small></i></Card.Title>
               </Card.Body>
-              <Card.Footer>{post.date} av {post.name}</Card.Footer>
+              <Card.Footer>{moment(post.post_Date).calendar()} av {post.user_Username}</Card.Footer>
             </Card>
             ))}
                 </div>
