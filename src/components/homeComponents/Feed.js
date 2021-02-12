1d530046451e6685f84eaa97fdc7ef48ee2bbd7e
ErLarsen <erikskrautlarsen@gmail.com>
2021-02-12 01:31:00 +0100

    Lage en funksjon som poster de 4 nyeste postene fra forumet (Feed.js)

--- src/components/homeComponents/Feed.js
+++ src/components/homeComponents/Feed.js
@@ -8,7 +8,7 @@ import moment from 'moment'
 const Feed = ({ post }) => {
     return (
       <div className="Feed">
-        {post.map((post, i) => (
+        {post.sort((p1, p2) => (moment(p2.post_Date).diff(moment(p1.post_Date)))).slice(0, 4).map((post, i) => (
             <Card key={i}>
               <Card.Body>
                 <Card.Title>{post.post_Title}<i><small> i {post.subTopic_Title}</small></i></Card.Title>
