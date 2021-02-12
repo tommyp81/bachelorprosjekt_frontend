78bfdcea4c13146a2de5de1e1925039317b83304
ErLarsen <erikskrautlarsen@gmail.com>
2021-02-12 01:29:07 +0100

    fixed warning (må adde key for hver listitem)

--- src/components/forumComponents/ForumCategories.js
+++ src/components/forumComponents/ForumCategories.js
@@ -5,27 +5,29 @@ import "./ForumCategories.css";
 const ForumCategories = ({ topics, subtopics }) => {
     return (
         <div className="ForumCategories">
-            {topics.map((topics) => (
             <Container>
-                <Accordion>
-                    <Row>
-                        <Card> 
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="success" size="lg" eventKey={topics.id}>
-                                    {topics.title}
-                                </Accordion.Toggle>
-                            </Card.Header>
-                        </Card>
-                    </Row>
-                    <Row>
-                        {subtopics.filter(subtopics => (subtopics.topicId === topics.id)).map(filteredSubtopics => (
-                        <Accordion.Collapse eventKey={filteredSubtopics.topicId}>
-                            <Card.Body><Button>{filteredSubtopics.title}</Button></Card.Body>
-                        </Accordion.Collapse>))}
-                    </Row>
-                </Accordion>
+                {topics.map((topics, i) => (
+                    
+                    <Accordion key={i}>
+                        <Row>
+                            <Card> 
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant="success" size="lg" eventKey={topics.id}>
+                                        {topics.title}
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                            </Card>
+                        </Row>
+                        <Row>
+                            {subtopics.filter(subtopics => (subtopics.topicId === topics.id)).map((filteredSubtopics, i) => (
+                            <Accordion.Collapse key={i} eventKey={filteredSubtopics.topicId}>
+                                <Card.Body><Button>{filteredSubtopics.title}</Button></Card.Body>
+                            </Accordion.Collapse>))}
+                        </Row>
+                    </Accordion>
+                    
+                ))}
             </Container>
-            ))}
         </div>
     );
 }
