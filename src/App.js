9419a97eceff33c398181b7bc0f5d019497eb07c
Sepitah <sepitah.1@gmail.com>
2021-02-18 18:22:18 +0100

    Making navigation sticky

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import { Container } from "react-bootstrap";
 import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
 
 //Importing main components.
@@ -18,24 +17,13 @@ import Home from "./components/homeComponents/Home.js";
 import Forum from "./components/forumComponents/Forum.js";
 import NewComment from "./components/forumComponents/NewComment.js";
 import Post from "./components/forumComponents/Post.js";
-
-//add npm i styled-components
-import styled from "styled-components";
 import { Navbar } from "./components/navigation/navbar/navbar";
 
 // https://webforum.azurewebsites.net/posts
 // https://webforum.azurewebsites.net/answers
 // https://webforum.azurewebsites.net/users
 
-const AppContainer = styled.div`
-width: 100%;
-height: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-background-color: rgba(15, 15, 15, 1);
-color: #fff;
-`;
+
 
 const App = () => {
  
@@ -64,9 +52,11 @@ const App = () => {
       <div className="App">
 
         {/* related to the navigationbar.*/}
-        <AppContainer>
+     
           <Navbar />
-        </AppContainer>
+       
+        
+      
         <Switch>
     
           <Route exact path="/" component={Home} />
