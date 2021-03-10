a3d443d5da7c52e63b3aeba71346a3578fb87985
Sepitah <sepitah.1@gmail.com>
2021-03-10 12:44:07 +0100

    name

--- src/components/loginComponents/Login.js
+++ src/components/loginComponents/Login.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react"; 
+import React, { useContext, useEffect, useState } from "react";
 import { Form, Button, Container, Col, Row } from "react-bootstrap";
-import WelcomeLogo from './WelcomeLogo';
+import WelcomeLogo from "./WelcomeLogo";
 
-import { useHistory } from 'react-router-dom'
-import Register from '../registerComponent/Register';
+import { useHistory } from "react-router-dom";
+import Register from "../registerComponent/Register";
 
 import "./Login.css";
 import { UserContext } from "../../UserContext";
@@ -12,8 +12,7 @@ const Login = ({ history }) => {
   const [uname, setUname] = useState("test");
   // const [password, setPassword] = useState("");
 
-
-  const { setUser } = useContext(UserContext)
+  const { setUser } = useContext(UserContext);
 
   function validateForm() {
     // return username.length > 0 && password.length > 0;
@@ -23,42 +22,39 @@ const Login = ({ history }) => {
     event.preventDefault();
     switch (uname) {
       case "test":
-        setUser({id: 6, username: "test", loggedIn: true})
+        setUser({ id: 6, username: "test", loggedIn: true });
         break;
       case "tommy":
-        setUser({id: 1, username: "tommy", loggedIn: true})
+        setUser({ id: 1, username: "tommy", loggedIn: true });
         break;
       case "henrik":
-        setUser({id: 2, username: "henrik", loggedIn: true})
+        setUser({ id: 2, username: "henrik", loggedIn: true });
         break;
       case "erik":
-        setUser({id: 3, username: "erik", loggedIn: true})
+        setUser({ id: 3, username: "erik", loggedIn: true });
         break;
-      case "pia": 
-        setUser({id: 4, username: "pia", loggedIn: true})
+      case "pia":
+        setUser({ id: 4, username: "pia", loggedIn: true });
         break;
       case "sepita":
-        setUser({id: 5, username: "sepita", loggedIn: true})
+        setUser({ id: 5, username: "sepideh", loggedIn: true });
         break;
       case "charlotte":
-        setUser({id: 7, username: "charlotte", loggedIn: true})
+        setUser({ id: 7, username: "charlotte", loggedIn: true });
       default:
         break;
     }
 
-    loginUser()
+    loginUser();
   }
 
   const loginUser = () => {
-    history.push("./")
-  }
+    history.push("./");
+  };
 
-  
-
-  
   return (
-  <div className="Login">
-    <Container fluid="md">
+    <div className="Login">
+      <Container fluid="md">
         <Row className="">
           <Col className="logo" sm={6}>
             <WelcomeLogo />
@@ -67,7 +63,7 @@ const Login = ({ history }) => {
             <h2>Logg inn med idrettens ID</h2>
             <a href="/Register">Ny bruker?</a>
             <Form onSubmit={handleSubmit}>
-                {/* <Form.Group controlId="formBasicEmail" >
+              {/* <Form.Group controlId="formBasicEmail" >
                     <Form.Control 
                     type="text"
                     name="username"
@@ -86,34 +82,36 @@ const Login = ({ history }) => {
                     onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group> */}
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                  <Form.Label>Velg Bruker</Form.Label>
-                  <Form.Control as="select" onChange={e => setUname(e.target.value)}>
-                    <option value="test">test</option>
-                    <option value="tommy">tommy</option>
-                    <option value="henrik">henrik</option>
-                    <option value="erik">erik</option>
-                    <option value="pia">pia</option>
-                    <option value="sepita">sepita</option>
-                    <option value="charlotte">charlotte</option>
-                  </Form.Control>
-                </Form.Group>
-                
-                <Button 
-                    variant="success" 
-                    type="submit"
-                    // disabled={!validateForm()}
+              <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Label>Velg Bruker</Form.Label>
+                <Form.Control
+                  as="select"
+                  onChange={(e) => setUname(e.target.value)}
                 >
-                    Logg inn
-                </Button>
+                  <option value="test">test</option>
+                  <option value="tommy">tommy</option>
+                  <option value="henrik">henrik</option>
+                  <option value="erik">erik</option>
+                  <option value="pia">pia</option>
+                  <option value="sepita">sepita</option>
+                  <option value="charlotte">charlotte</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Button
+                variant="success"
+                type="submit"
+                // disabled={!validateForm()}
+              >
+                Logg inn
+              </Button>
             </Form>
             <p>Glemt passord?</p>
           </Col>
         </Row>
       </Container>
-    </div> 
-    
+    </div>
   );
-}
+};
 
 export default Login;
