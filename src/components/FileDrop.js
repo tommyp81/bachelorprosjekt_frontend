d6d1b3f0282ee75b857f8a150dd7b8f51b8d285f
ErLarsen <erikskrautlarsen@gmail.com>
2021-03-18 20:14:55 +0100

    maxfilesize + messages

--- src/components/FileDrop.js
+++ src/components/FileDrop.js
@@ -16,28 +16,46 @@ const acceptDropStyle = {
   color: '#00e676'
 }
 
+const rejectDropStyle = {
+  color: '#ff1744'
+}
+
 
 const FileDrop = ({ file, setFile }) => {
 
 
   const onDrop = useCallback(acceptedFiles => setFile(acceptedFiles[0]))
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+  const {acceptedFiles, fileRejections, getRootProps, getInputProps} = useDropzone({
     onDrop,
-    maxFiles: 1
+    maxFiles: 1,
+    maxSize: 104857600
   });
 
   const fileAccepted = Boolean(file);
+  const fileRejected = fileRejections.length > 0
 
   const style = useMemo(() => ({
     ...dropStyle,
-    ...fileAccepted ? acceptDropStyle : {}
-  }), [fileAccepted])
+    ...fileAccepted ? acceptDropStyle : {},
+    ...fileRejected ? rejectDropStyle : {}
+  }), [fileAccepted, fileRejected])
+
+  const dropzoneMessage = () => {
+    if (fileAccepted) {
+      return <p>{file.name} &#10004;&#65039;</p>
+    } else if (fileRejected) {
+      return <p>Filen er for stor &#10060;</p>
+    } else {
+      return <p>Drag'n'drop fil, eller klikk for å velge en fil. (100MB MAX)</p>
+    }
+  }
 
   return (
     <div>
       <div {...getRootProps({style})}>
         <input {...getInputProps()} />
-        {file ? <p>{file.name} &#10003;</p> : <p>Drag'n'drop fil, eller klikk for å velge en fil.</p> }
+        {/* {file ? <p>{file.name} &#10003;</p> : <p>Drag'n'drop fil, eller klikk for å velge en fil.</p> } */}
+        {dropzoneMessage()}
       </div>
     </div>
   )
