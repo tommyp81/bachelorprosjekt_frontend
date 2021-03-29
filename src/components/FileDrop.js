5616b6b64d2c091da13084c11fd8e5267872b0ca
Sepitah <sepitah.1@gmail.com>
2021-03-29 20:44:10 +0200

    File extension restriction + 15 MB MaxSize

--- src/components/FileDrop.js
+++ src/components/FileDrop.js
@@ -1,65 +1,73 @@
-import React, {useCallback, useMemo} from 'react'
-import {useDropzone} from 'react-dropzone';
+import React, { useCallback, useMemo } from "react";
+import { useDropzone } from "react-dropzone";
 
 const dropStyle = {
-  textAlign: 'center',
-  padding: '10px',
-  borderWidth: '3px',
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
+  textAlign: "center",
+  padding: "10px",
+  borderWidth: "3px",
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
   marginBottom: "20px",
-  borderRadius: "3px"
-}
+  borderRadius: "3px",
+};
 
 const acceptDropStyle = {
-  color: '#00e676'
-}
+  color: "#00e676",
+};
 
 const rejectDropStyle = {
-  color: '#ff1744'
-}
-
+  color: "#ff1744",
+};
 
 const FileDrop = ({ file, setFile }) => {
-
-
-  const onDrop = useCallback(acceptedFiles => setFile(acceptedFiles[0]))
-  const {acceptedFiles, fileRejections, getRootProps, getInputProps} = useDropzone({
+  const onDrop = useCallback((acceptedFiles) => setFile(acceptedFiles[0]));
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps,
+  } = useDropzone({
     onDrop,
     maxFiles: 1,
-    maxSize: 104857600
+    maxSize: 15728640,
+    accept: " .txt ,.pdf, .jpg, .jpeg, .png, .doc, .docx, .xls, .xlsx",
   });
 
   const fileAccepted = Boolean(file);
-  const fileRejected = fileRejections.length > 0
+  const fileRejected = fileRejections.length > 0;
 
-  const style = useMemo(() => ({
-    ...dropStyle,
-    ...fileAccepted ? acceptDropStyle : {},
-    ...fileRejected ? rejectDropStyle : {}
-  }), [fileAccepted, fileRejected])
+  const style = useMemo(
+    () => ({
+      ...dropStyle,
+      ...(fileAccepted ? acceptDropStyle : {}),
+      ...(fileRejected ? rejectDropStyle : {}),
+    }),
+    [fileAccepted, fileRejected]
+  );
 
   const dropzoneMessage = () => {
     if (fileAccepted) {
-      return <p>{file.name} &#10004;&#65039;</p>
+      return <p>{file.name} &#10004;&#65039;</p>;
     } else if (fileRejected) {
-      return <p>Filen er for stor &#10060;</p>
+      return <p>Filen er for stor eller Filtypen er ugyldig &#10060;</p>;
     } else {
-      return <p>Drag'n'drop, eller klikk for å velge en fil (Max 100 megabytes)</p>
+      return (
+        <p>Drag'n'drop, eller klikk for å velge en fil (Max 15 megabytes)</p>
+      );
     }
-  }
+  };
 
   return (
     <div>
-      <div {...getRootProps({style})}>
+      <div {...getRootProps({ style })}>
         <input {...getInputProps()} />
         {/* {file ? <p>{file.name} &#10003;</p> : <p>Drag'n'drop fil, eller klikk for å velge en fil.</p> } */}
         {dropzoneMessage()}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FileDrop
+export default FileDrop;
