ae6aafedcb8997e22a7f20098f24d626d861eb7d
Sepitah <sepitah.1@gmail.com>
2021-04-05 12:44:48 +0200

    UploadFile Validation

--- src/components/infoComponents/UploadFile.js
+++ src/components/infoComponents/UploadFile.js
@@ -11,18 +11,30 @@ import {
 } from "react-bootstrap";
 import FileDrop from "../FileDrop";
 import validator from "validator";
-const UploadFile = ({infoTopics}) => {
+const UploadFile = ({ infoTopics }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [errorMessage, setErrorMessage] = useState("");
+  const [validated, setValidated] = useState(false);
 
-  const validateUrl = (value) => {
-    if (validator.isURL(value)) {
-      setErrorMessage("Gyldig URL");
+  const validateUrl = (event) => {
+    if (validator.isURL(event)) {
+      setValidated(true);
     } else {
       setErrorMessage("Ugyldig URL");
+      setValidated(false);
     }
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
   };
 
   return (
@@ -38,13 +50,19 @@ const UploadFile = ({infoTopics}) => {
         <Tabs defaultActiveKey="0">
           <Tab title="Video" eventKey="0">
             <Modal.Body>
-              <Form onChange={(e) => validateUrl(e.target.value)}>
-                <Form.Label>Lim inn YouTube-URL *</Form.Label>
-                <Form.Control type="input"></Form.Control>
+              <Form
+                noValidate
+                validated={validated}
+                onChange={(e) => validateUrl(e.target.value)}
+              >
+                <Form.Label>Lim inn YouTube-URL</Form.Label>
+                <Form.Control type="input" required></Form.Control>
+                <span
+                  style={{ fontSize: 11, fontWeight: "bold", color: "red" }}
+                >
+                  {errorMessage}
+                </span>
               </Form>
-              <span style={{ fontSize: 11, fontWeight: "bold", color: "red" }}>
-                {errorMessage}
-              </span>
             </Modal.Body>
           </Tab>
           <Tab title="Dokument" eventKey="1">
@@ -54,22 +72,37 @@ const UploadFile = ({infoTopics}) => {
           </Tab>
         </Tabs>
         <Modal.Body>
-          <Form>
-             <Form.Label>Tittel</Form.Label>
-            <Form.Control type="input"></Form.Control>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Label>Tittel</Form.Label>
+            <Form.Control type="input" required></Form.Control>
+            <Form.Control.Feedback
+              type="invalid"
+              style={{ fontSize: 11, fontWeight: "bold", color: "red" }}
+            >
+              Tittel kreves!
+            </Form.Control.Feedback>
             <Form.Label>Beskrivelse</Form.Label>
-            <Form.Control as="textarea" rows={3}></Form.Control>
-               <Form.Control as="select">{infoTopics.map((mappedInfoTopics) => (
-              <option>{mappedInfoTopics.title}</option> ))}
+            <Form.Control as="textarea" rows={3} required></Form.Control>
+            <Form.Control.Feedback
+              type="invalid"
+              style={{ fontSize: 11, fontWeight: "bold", color: "red" }}
+            >
+              Beskrivelse kreves!
+            </Form.Control.Feedback>
+            <Form.Control as="select" custom>
+              {infoTopics.map((mappedInfoTopics) => (
+                <option>{mappedInfoTopics.title}</option>
+              ))}
             </Form.Control>
-           
-           
-          </Form></Modal.Body>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="danger" onClick={handleClose}>
             Avbryt
           </Button>
-          <Button variant="primary">Last opp</Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Last opp
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>
