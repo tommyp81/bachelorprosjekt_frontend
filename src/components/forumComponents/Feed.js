8bd983cf1880de60a19834babbc690378a2a8b4f
Sepitah <sepitah.1@gmail.com>
2021-04-19 14:18:02 +0200

    Les Mer

--- src/components/forumComponents/Feed.js
+++ src/components/forumComponents/Feed.js
@@ -1,43 +1,104 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { Card, Button, Spinner } from "react-bootstrap";
-import moment from 'moment'
-import 'moment/locale/nb'
-import { FaThumbsUp, FaRegComment } from 'react-icons/fa'
-import { Link } from 'react-router-dom';
-import SpinnerDiv from './SpinnerDiv';
+import moment from "moment";
+import "moment/locale/nb";
+import { FaThumbsUp, FaRegComment } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import SpinnerDiv from "./SpinnerDiv";
 
-import './Feed.css'
+import "./Feed.css";
 
-const Feed = ({ posts, users, topic, subtopic, maxLength, loading}) => {
+const Feed = ({ posts, users, topic, subtopic, maxLength, loading }) => {
+  const [ReadMore, setReadMore] = useState(false);
+  const getText = (content) => {
+    if (content.length <= 150) return content;
 
+    if (content.length > 150 && ReadMore) {
+      return (
+        <>
+          <Card.Text>
+            {content}
+            <Link variant="link" onClick={() => setReadMore(false)}>
+              {" "}
+              Les Mindre
+            </Link>
+          </Card.Text>
+        </>
+      );
+    }
 
-  if (posts.length === 0 && users.length === 0 && topic.length === 0 && subtopic.length === 0) {
-    console.log("HEI")
-    return <SpinnerDiv/>
+    if (content.length > 150) {
+      return (
+        <>
+          <Card.Text>
+            {content.slice(0, 150)}
+            <Link variant="link" onClick={() => setReadMore(true)}>
+              {" "}
+              ... Les Mer
+            </Link>
+          </Card.Text>
+        </>
+      );
+    }
+  };
+
+  if (
+    posts.length === 0 &&
+    users.length === 0 &&
+    topic.length === 0 &&
+    subtopic.length === 0
+  ) {
+    console.log("HEI");
+    return <SpinnerDiv />;
   }
-
 
   return (
     <div className="Feed">
       {posts.slice(0, maxLength).map((post, i) => (
         <Card key={i}>
-          <Link to={`/forum/${post.id}`} style={{ textDecoration: 'none', color: '#000000' }}>
+          <Link
+            to={`/forum/${post.id}`}
+            style={{ textDecoration: "none", color: "#000000" }}
+          >
             <Card.Body>
               <div className="float-left">
-                {users.filter(user => (user.id === post.userId)).map((filteredUser, i) => (
-                  <Card.Text key={i}>Postet av <b>{filteredUser.username}</b> {moment(post.date).calendar()}</Card.Text>
-                ))}
+                {users
+                  .filter((user) => user.id === post.userId)
+                  .map((filteredUser, i) => (
+                    <Card.Text key={i}>
+                      Postet av <b>{filteredUser.username}</b>{" "}
+                      {moment(post.date).calendar()}
+                    </Card.Text>
+                  ))}
               </div>
-              <br /><br />
+              <br />
+              <br />
               <Card.Title>{post.title}</Card.Title>
-                {topic.filter(topic => (topic.id === post.topicId)).map((filteredTopics, j) => (
+              {topic
+                .filter((topic) => topic.id === post.topicId)
+                .map((filteredTopics, j) => (
                   <Card.Text key={j} className="float-left">
-                    {filteredTopics.title} - {subtopic.find(st => st.id === post.subTopicId)?.title}
+                    {filteredTopics.title} -{" "}
+                    {subtopic.find((st) => st.id === post.subTopicId)?.title}
                   </Card.Text>
-                ))}              
+                ))}
               <Card.Text className="float-right">
-              {post.like_Count} <FaThumbsUp className="like ml-1 mr-1 mb-2" color="grey" size={18} /> &nbsp;
-              {post.comment_Count} <FaRegComment size={18} color="grey" className="ml-2 mr-2 mb-1"/> 
+                {post.like_Count}{" "}
+                <FaThumbsUp
+                  className="like ml-1 mr-1 mb-2"
+                  color="grey"
+                  size={18}
+                />{" "}
+                &nbsp;
+                {post.comment_Count}{" "}
+                <FaRegComment
+                  size={18}
+                  color="grey"
+                  className="ml-2 mr-2 mb-1"
+                />
+              </Card.Text>
+              <Card.Text className="float-right">
+                {getText(post.content)}
               </Card.Text>
             </Card.Body>
           </Link>
@@ -45,6 +106,6 @@ const Feed = ({ posts, users, topic, subtopic, maxLength, loading}) => {
       ))}
     </div>
   );
-}
+};
 
 export default Feed;
