6549672e5e174e4498b59c815e2e4bf9e65465f7
Sepitah <sepitah.1@gmail.com>
2021-05-06 11:25:07 +0200

    Optimization

--- src/components/Admin/PasswordDialog.js
+++ src/components/Admin/PasswordDialog.js
@@ -1,79 +1,87 @@
-import React, { useState } from 'react'
-import { Button, Form, Modal} from 'react-bootstrap'
-import { host } from '../../App'
+import React, { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { host } from "../../App";
 
 const PasswordDialog = ({ user }) => {
-
-  const [password, setPassword] = useState("")
-  const [password2, setPassword2] = useState("")
-
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    setPassword("")
-    setPassword2("")
-    setShow(true)
-  }
+    setPassword("");
+    setPassword2("");
+    setShow(true);
+  };
 
-  const isDiabled = password.length < 1 || password2.length < 1
-
+  const isDiabled = password.length < 1 || password2.length < 1;
 
   const setNewPassword = async (e) => {
     e.preventDefault();
-    if(password === password2) {
-      const res = await fetch(host+`users/${user.id}`, {
-        method: 'PUT',
+    if (password === password2) {
+      const res = await fetch(host + `users/${user.id}`, {
+        method: "PUT",
         headers: {
-          'content-type': 'application/json'
+          "content-type": "application/json",
         },
-        body: JSON.stringify({...user, password})
-      })
-      if(res.ok) {
+        body: JSON.stringify({ ...user, password }),
+      });
+      if (res.ok) {
         handleClose();
-        alert("Passordet er endret!")
+        alert("Passordet er endret!");
       } else {
-        const text = await res.text()
-        alert(text)
+        const text = await res.text();
+        alert(text);
       }
-
     } else {
-      alert("Passordene er ikke like!")
+      alert("Passordene er ikke like!");
     }
-    
-  }
-  
+  };
+
   return (
     <>
-      <span className="btn-link" onClick={handleShow}>Endre Passord</span>
+      <span className="btn-link" onClick={handleShow}>
+        Endre Passord
+      </span>
       <Modal animation={false} show={show} onHide={handleClose}>
         <Modal.Header closeButton={true}>
           <Modal.Title>Sett nytt passord for: {user.username}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form.Control 
-            type='password'
-            placeholder='Nytt Passord'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <Form.Control 
-            type='password'
-            placeholder='Bekreft Passord'
-            value={password2}
-            onChange={e => setPassword2(e.target.value)}
-          />
-          <div className="float-right">
-            <Button disabled={isDiabled} variant="success" onClick={setNewPassword} >OK</Button>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              placeholder="Nytt Passord"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              placeholder="Bekreft Passord"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+            />
+          </Form.Group>
+        </Modal.Body>
+        <div className="float-right">
+          <Modal.Footer>
+            <Button
+              disabled={isDiabled}
+              variant="success"
+              onClick={setNewPassword}
+            >
+              OK
+            </Button>
             <Button variant="danger" onClick={handleClose}>
               Avbryt
             </Button>
-          </div>
-        </Modal.Body>
-
+          </Modal.Footer>
+        </div>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default PasswordDialog
+export default PasswordDialog;
--- src/components/Admin/UsernameDialog.js
+++ src/components/Admin/UsernameDialog.js
@@ -1,84 +1,103 @@
-import React, { useContext, useState } from 'react'
-import { Button, Form, Modal } from 'react-bootstrap'
-import { host } from '../../App'
-import { UserContext } from '../../UserContext'
+import React, { useContext, useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { host } from "../../App";
+import { UserContext } from "../../UserContext";
 
-const UsernameDialog = ({users, setUsers}) => {
+const UsernameDialog = ({ users, setUsers }) => {
+  const { user, setUser } = useContext(UserContext);
 
-  const {user, setUser} = useContext(UserContext)
-
-  const [newUsername, setNewusername] = useState(user.username)
-  const [password, setPassword] = useState("")
+  const [newUsername, setNewusername] = useState(user.username);
+  const [password, setPassword] = useState("");
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    setPassword("")
-    setShow(true)
-  }
+    setPassword("");
+    setShow(true);
+  };
 
-  const isDisabled = (newUsername.length < 3 || newUsername === user.username) || !password;
-  
+  const isDisabled =
+    newUsername.length < 3 || newUsername === user.username || !password;
 
   const changeUsername = async () => {
     // e.preventDefault();
-    const res = await fetch(host+`users/${user.id}`, {
-      method: 'PUT',
+    const res = await fetch(host + `users/${user.id}`, {
+      method: "PUT",
       headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json",
       },
-      body: JSON.stringify({...users.find(u => u.id == user.id), username: newUsername, password})
-    })
-    if(res.ok) { 
-      const data = await res.json()
-      setUser({...user, username: data.username})
-      setUsers(users.map(u => {
-        if(u.id == user.id) {
-          return {
-            ...u, 
-            username: data.username
+      body: JSON.stringify({
+        ...users.find((u) => u.id == user.id),
+        username: newUsername,
+        password,
+      }),
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setUser({ ...user, username: data.username });
+      setUsers(
+        users.map((u) => {
+          if (u.id == user.id) {
+            return {
+              ...u,
+              username: data.username,
+            };
           }
-        }
-        return u
-      }))
-      alert("Brukernavn endret!")
+          return u;
+        })
+      );
+      alert("Brukernavn endret!");
     } else {
-      const text = await res.text()
-      alert(text)
+      const text = await res.text();
+      alert(text);
     }
-    setPassword("")
-  }
+    setPassword("");
+  };
 
   return (
     <>
-      <span className="btn-link" onClick={handleShow}>Endre Brukernavn</span>
+      <span className="btn-link" onClick={handleShow}>
+        Endre Brukernavn
+      </span>
       <Modal animation={false} show={show} onHide={handleClose}>
         <Modal.Header closeButton={true}>
           <Modal.Title>Sett nytt Brukernavn for: {user.username}</Modal.Title>
         </Modal.Header>
-        <div>
-          <Form.Control 
-            type='text'
-            placeholder='Nytt Brukernavn'
-            value={newUsername}
-            onChange={e => setNewusername(e.target.value)}
-          />
-          <Form.Control 
-            type='password'
-            placeholder='Passord'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <div className="float-right">
-            <Button disabled={isDisabled} variant="success" onClick={changeUsername}>Bekreft</Button>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="Nytt Brukernavn"
+              value={newUsername}
+              onChange={(e) => setNewusername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              placeholder="Passord"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+        </Modal.Body>
+        <div className="float-right">
+          <Modal.Footer>
+            <Button
+              disabled={isDisabled}
+              variant="success"
+              onClick={changeUsername}
+            >
+              Bekreft
+            </Button>
             <Button variant="danger" onClick={handleClose}>
               Lukk
             </Button>
-          </div>
+          </Modal.Footer>
         </div>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default UsernameDialog
+export default UsernameDialog;
