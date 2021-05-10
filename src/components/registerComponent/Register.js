482559d2954e636bbe579588307dd0f9ca04a9e4
Pia Karoline <pkaamodt@gmail.com>
2021-05-10 11:31:25 +0200

    Validering av brukerregistrering, trenger hjelp

--- src/components/registerComponent/Register.js
+++ src/components/registerComponent/Register.js
@@ -5,11 +5,22 @@ import { host } from "../../App";
 const Register = ({ setTabKey, setUsers, loginUser}) => {
 
   const [username, setUsername] = useState("");
+  const [checkUsername, setCheckUsername] = useState("");
+
   const [firstname, setFirstname] = useState("");
+  const [checkFirstname, setCheckFirstname] = useState("");
+
   const [lastname, setLastname] = useState("");
+  const [checkLastname, setCheckLastname] = useState("");
+
   const [email, setEmail] = useState("");
+  const [checkEmail, setCheckEmail] = useState("");
+
   const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("")
+
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [checkConfirmPassword, setCheckConfirmPassword] = useState("")
 
   var isDisabled = true;
   if(!username || !firstname || !lastname || !email || !password || !confirmPassword){
@@ -57,13 +68,51 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
         .catch((error) => console.log(error));
       }
 
-      
+    /*
+    const validUsername = new RegExp(/^[a-zA-Z]{3,20}\d*$/i) 
+    if (!username.match(validUsername)) {
+      setCheckUsername(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Brukernavn må være minst 3 og <br/>maks 20 karakterer langt</span>)
+    } else if (sjekk om brukernavn eksisterer) {
+      setCheckUsername(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Brukernavnet er opptatt</span>)
+    } else {
+      setCheckUsername("")
+    }
+
+    const validEmail = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+    if (!email.match(validEmail)) {
+      setCheckEmail(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>E-posten er ikke gyldig</span>)
+    } else if (sjekk om epost eksisterer) {
+      setCheckEmail(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>E-posten er opptatt</span>)
+    } else {
+      setCheckEmail("")
+    }
+
+    if (firstname < 0) {
+      setCheckFirstname(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Fornavn kan ikke være tomt!</span>)
+    } else {
+      setCheckFirstname("")
+    }
+
+    if (lastname < 0) {
+      setCheckLastname(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Etternavn kan ikke være tomt!</span>)
+    } else {
+      setCheckLastname("")
+    }
+
+    /*
+    const validPassword = new RegExp(/"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/)
+    if (!password.match(validPassword)) {
+      setCheckPassword(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Passord må være minst 8 karakterer langt <br/>og ha minst ett tall</span>)
+    }
+    }*/
     if(password === confirmPassword && !userExists){
+      setCheckConfirmPassword("")
       setTabKey('login')
     } else {
-      alert("Passord og bekreft passord er ikke like.")
+      setCheckConfirmPassword(<span style={{color: "red", fontSize: "13px",fontWeight: "600"}}>Passord og bekreft passord er ikke like!</span>)
     }
   };
+
 
   return (
     <div className="Register">
@@ -78,6 +127,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 onChange={(e) => setUsername(e.target.value)}
                 tabIndex="1"
               />
+              {checkUsername}
               <br />
               <Form.Label>Fornavn</Form.Label>
               <Form.Control
@@ -86,6 +136,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 onChange={(e) => setFirstname(e.target.value)}
                 tabIndex="3"
               />
+              {checkFirstname}
               <br/>
               <Form.Label>Passord</Form.Label>
               <Form.Control
@@ -94,6 +145,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                   onChange={(e) => setPassword(e.target.value)}
                   tabIndex="5"
                 />
+              {checkPassword}
               <br/>
             </Form>
             </Col>
@@ -106,6 +158,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 onChange={(e) => setEmail(e.target.value)}
                 tabIndex="2"
               />
+              {checkEmail}
               <br/>
               <Form.Label>Etternavn</Form.Label>
               <Form.Control
@@ -114,6 +167,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 onChange={(e) => setLastname(e.target.value)}
                 tabIndex="4"
               />
+              {checkLastname}
               <br/>
               
               <Form.Label>Bekreft passord</Form.Label>
@@ -123,6 +177,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 tabIndex="6"
               />
+              {checkConfirmPassword}
               <br/>
             </Form>
             </Col>
@@ -153,6 +208,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
+              {checkUsername}
               <br />
               <Form.Label>Fornavn</Form.Label>
               <Form.Control
@@ -160,6 +216,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 value={firstname}
                 onChange={(e) => setFirstname(e.target.value)}
               />
+              {checkFirstname}
               <br/>
               <Form.Label>Etternavn</Form.Label>
               <Form.Control
@@ -167,6 +224,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 value={lastname}
                 onChange={(e) => setLastname(e.target.value)}
               />
+              {checkLastname}
               <br/>
               
             </Form>
@@ -179,6 +237,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              {checkEmail}
               <br/>
               <Form.Label>Passord</Form.Label>
               <Form.Control
@@ -186,6 +245,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
+                {checkPassword}
               <br/>
               <Form.Label>Bekreft passord</Form.Label>
               <Form.Control
@@ -193,6 +253,7 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
+              {checkConfirmPassword}
               <br/>
             </Form>
             </Col>
@@ -209,85 +270,6 @@ const Register = ({ setTabKey, setUsers, loginUser}) => {
               </Button>
             </Col>
           </Row></div>
-     {/*
-      <Container fluid="md">
-        <Row>
-          <Col className="logo" sm={12} >
-                        <WelcomeLogo />
-                        </Col>
-          <Col className="submit">
-            <Form onSubmit={handleSubmitUser}>
-              <Form.Group >
-                <Form.Label>Brukernavn</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Brukernavn"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group >
-                <Form.Label>Fornavn</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Fornavn"
-                  value={firstname}
-                  onChange={(e) => setFirstname(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group >
-                <Form.Label>Etternavn</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Etternavn"
-                  value={lastname}
-                  onChange={(e) => setLastname(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group >
-                <Form.Label>E-post</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group >
-                <Form.Label>Passord</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Passord"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group >
-                <Form.Label>Bekreft passord</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Bekreft Passord"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </Form.Group>
-
-              <Button
-                variant="success"
-                type="submit"
-                disabled={isDisabled}
-              >
-                Opprett bruker
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>*/}
     </div>
   );
 };
